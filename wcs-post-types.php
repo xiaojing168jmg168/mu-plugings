b4d61916309d996de6336cce28377520ff120123
Xiaojing Deng <8redfishcreative@gmail.com>
2023-04-26 11:04:55 -0400

    must use plugins

---
+++ wcs-post-types.php
@@ -0,0 +1,53 @@
+<?php
+function wcs_post_types(){
+    // Event post type
+    register_post_type('event', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor','excerpt'),
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
+
+    // Program post type
+    register_post_type('program', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
+    ));
+
+     // Instructors post type
+     register_post_type('instructor', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'public' => true,
+        'labels' => array(
+            'name' => 'Instructors',
+            'add_new_item' => 'Add New Instructor',
+            'edit_item' => 'Edit Instructor',
+            'all_items' => 'All Instructors',
+            'singular_name' => 'Instructor'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more'
+    ));
+}
+
+add_action('init', 'wcs_post_types');
